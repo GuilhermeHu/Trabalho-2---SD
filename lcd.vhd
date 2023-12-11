95f018262421e697fe3b2e9c5faff7843664db96
GuilhermeHu <149438896+GuilhermeHu@users.noreply.github.com>
2023-12-11 20:32:42 -0300

    Update lcd.vhd

--- lcd.vhd
+++ lcd.vhd
@@ -34,32 +34,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --Link do vídeo: https://youtu.be/tbBiHPvzUIg?si=jQWMAsOxOqRG1Lmw
 
 entity LCD is
-generic (fclk: natural := 110_000_000); -- 50MHz , cristal do kit EE03			--Alterador da frequência do clock
+generic (fclk: natural := 110_000_000); -- 50MHz , cristal do kit EE03			
 		port (chute : in STD_LOGIC_VECTOR (2 downto 0);				--Entrada da forca: algarismo chutado pelo jogador (em binário)
 		      botao : in STD_LOGIC;						--Botão: confirmação do chute botado nos switchs (input), de modo a mandar o chute ao jogo
 		      reset : in STD_LOGIC;						--Botão de Reset: reinicia o jogo inteiro
 		      clk   : in STD_LOGIC;						--Entrada de clock
 		      ledvidas:  out std_logic_vector(2 downto 0); 			--Saída dos LEDS da placa FPGA, que serão proporcionais às vidas que o jogador possui
-		      RS, RW      : out bit;						--Saídas do lcd da placa FPGA, que definem se a memória 
-		      E           : buffer bit;  					
-		      DB          : out bit_vector(7 downto 0)); 
+		      RS, RW      : out bit;						--RS = Register Select (Habilitação da memória) // RW = Read/Write Control (1 para habilitação da leitura, 0 para habilitação da escrita)
+		      E           : buffer bit;  					--Read/Write Enable Pulse (indica que a memória está habilitada para armazenar dados)
+		      DB          : out bit_vector(7 downto 0)); 			--Vetor dos data bits, os bits que são recebidos pela memória e podem aparecer no display lcd
 end LCD;
 
-architecture arq_LCD of LCD is
+architecture arq_LCD of LCD is																	--Esses estados foram criados pelo autor original desse código
 	type state is (FunctionSetl, FunctionSet2, FunctionSet3,
-	 FunctionSet4,FunctionSet5,FunctionSet6,FunctionSet7,FunctionSet8,FunctionSet9,FunctionSet10,FunctionSet11,FunctionSet12,
-	 FunctionSet13,FunctionSet14,FunctionSet15,FunctionSet16,FunctionSet17,FunctionSet18,FunctionSet19,ClearDisplay, DisplayControl, EntryMode, 
-	 WriteDatal, WriteData2, WriteData3, WriteData4, WriteData5,WriteData6,WriteData7,WriteData8,WriteData9,WriteData10,WriteData11,
-	 WriteData12,SetAddress,SetAddress1, ReturnHome);
+	 FunctionSet4,FunctionSet5,FunctionSet6,FunctionSet7,FunctionSet8,FunctionSet9,FunctionSet10,FunctionSet11,FunctionSet12,				--Declaração dos estados do módulo do lcd, para
+	 FunctionSet13,FunctionSet14,FunctionSet15,FunctionSet16,FunctionSet17,FunctionSet18,FunctionSet19,ClearDisplay, DisplayControl, EntryMode, 		--manipulação da memória do lcd. Os estados podem
+	 WriteDatal, WriteData2, WriteData3, WriteData4, WriteData5,WriteData6,WriteData7,WriteData8,WriteData9,WriteData10,WriteData11,			--realizar o set, leitura ou escrita na memória,
+	 WriteData12,SetAddress,SetAddress1, ReturnHome);													--para que seus valores apareçam no display lcd
 	
-	signal pr_state, nx_state: state; 
+	signal pr_state, nx_state: state; 	      --signal que representa a transição de estados
 	signal comp : std_logic_vector(4 downto 0);   --signal do vetor que apresenta quais os algarismos da senha já foram acertados
 	signal gpsig : std_logic_vector(1 downto 0);  --signal do vetor gp. Caso gp(1)=1, o jogador ganhou o jogo; caso gp=0, o jogador perdeu o jogo
-	signal clkm : std_logic;
+	signal clkm : std_logic;		      --um clock extra para funcionar dentro do component "forca" e fazer sua transição de estados
 	
-Component forca is
-    Port ( chute : in  STD_LOGIC_VECTOR (2 downto 0);    
-           botao : in  STD_LOGIC;
+Component forca is						--Declaração do component da forca, que realiza o jogo da forca.
+    Port ( chute : in  STD_LOGIC_VECTOR (2 downto 0);    	--Suas saídas são trazidas para esse módulo de lcd para saber
+           botao : in  STD_LOGIC;				--o que deve ser mostrado no display lcd e na placa FPGA
 	   reset : in std_logic;
 	   clk   : in std_logic;
 	   vidasled: out std_logic_vector(2 downto 0);
@@ -69,13 +69,13 @@ Component forca is
 end component;
 begin
 
-forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);
+forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);  --Chamada do component da forca
 
 ---—Clock generator (E->500Hz) :---------
-		process (clk)
-		variable count: natural range 0 to fclk/100; 
-		begin
-			if (clk' event and clk = '1') then 
+		process (clk)					--Alterador da frequência do clock.
+		variable count: natural range 0 to fclk/100; 	--Esse novo clock é usado para reger a transição dos estados para a manipulação  
+		begin						--da memória do display lcd [E] e para a transição dos estados do jogo da forca (em
+			if (clk' event and clk = '1') then 	--seu respectivo component) [clkm]
 				count := count + 1;
 				if (count=fclk/100) then 
 				 E <= not E; 
@@ -85,8 +85,8 @@ forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);
 			end if; 
 		end process;
 
------Lower section of FSM:---------------
-		process (E) 
+-----Lower section of FSM:---------------	
+		process (E) 					--Realiza a transição de estados
 		begin
 			if (E' event and E = '1') then 
 				pr_state <= FunctionSetl; 
@@ -100,12 +100,12 @@ forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);
 		case pr_state is
 
 
-		when FunctionSetl => 
-		RS<= '0'; RW<= '0'; 
-		DB<= "00111000"; 
-		nx_state <= FunctionSet2; 
-		
-		when FunctionSet2 => 
+		when FunctionSetl => 		--Os estados FunctionSetl, FunctionSet2, FunctionSet3, FunctionSet4, FunctionSet5, FunctionSet6,		
+		RS<= '0'; RW<= '0'; 		--FunctionSet7, FunctionSet8, FunctionSet9, FunctionSet10, FunctionSet11, FunctionSet12, FunctionSet13,	
+		DB<= "00111000"; 		--FunctionSet14, FunctionSet15, FunctionSet16, FunctionSet17, FunctionSet18 e FunctionSet19 são
+		nx_state <= FunctionSet2; 	--responsáveis por setar as memórias do lcd.
+						--Essa parte do código não foi mudada em relação ao código original do canal "WR Kits", até o início
+		when FunctionSet2 => 		--dos estados de WriteData.
 		RS<= '0'; RW<= '0'; 
 		DB <= "00111000";
 		nx_state <= FunctionSet3; 
@@ -222,13 +222,13 @@ forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);
 		nx_state  <= WriteData2; 
 		
 
-		when WriteData2 =>
-		RS<= '1'; RW<= '0';
-		if (comp(4) = '1') then
-			DB <= X"37"; --7
-		else
-			DB <= X"2D"; -- _
-		end if;
+		when WriteData2 =>		--A partir daqui as alterações no código foram feitas, de modo a definir o que seria mostrado no display lcd.
+		RS<= '1'; RW<= '0';		--O vetor "comp" apresenta quais posições da senha já foram acertadas durante o jogo. Dessa forma, nesse módulo
+		if (comp(4) = '1') then		--de print no display lcd, dependendo se a posição do bit no vetor "comp" for 1 ou 0, será mostrado no lcd, 
+			DB <= X"37"; --7	--respectivamente, o algarismo acertado (que nem na forca) ou o símbolo "_" (algarismo ainda não foi acertado).
+		else				--Por exemplo, nesse estado "WriteData2", caso a posição mais significativa do vetor "comp" esteja em nível alto,
+			DB <= X"2D"; -- _	--será printado o algarismo 7, que é o valor mais significativo da senha; caso contrário, será printado "_". Essa
+		end if;				--lógica foi aplicada dos estados "WriteData2" ao "WriteData6", para todos os cinco dígitos da senha.
 		nx_state <= WriteData3; 
 		
 		when WriteData3 =>
@@ -272,12 +272,12 @@ forca_f: forca port map (chute,botao,reset,clkm,ledvidas,comp,gpsig);
 		DB   <=  "11000101";         --COMANDO PARA POSICIONAR O CURSOR NA LINHA 2 COLUNA 6
 		nx_state  <= WriteData7;
 
-		when  WriteData7   =>
-		RS<=   '1';   RW<=   '0';
-		if (gpsig = "00") then
-			DB <= X"20"; -- espaço
-		elsif (gpsig = "01") then
-			DB <= X"50"; --P
+		when  WriteData7   =>			--Os estados "WriteData7" ao "WriteData12" ficam responsáveis pelo print caso aconteça alguma condição
+		RS<=   '1';   RW<=   '0';		--de fim de jogo (vitória ou derrota). Nesse caso, é analisado o vetor gpsig: caso seu valor seja "00",
+		if (gpsig = "00") then			--não aconteceu nenhuma condição de fim de jogo e este continua, nada sendo printado na segunda linha
+			DB <= X"20"; -- espaço		--do lcd (por isso space).; caso gpsig = "10" = g = vitória, os estados "WriteData7" ao "WriteData12" 
+		elsif (gpsig = "01") then		-- mostrarão no lcd a palavra "Ganhou"; por fim, caso gpsig = "01" = p = derrota, os estados printarão
+			DB <= X"50"; --P		--"Perdeu"
 		else
 			DB <= X"47"; --G
 		end if;
